e753153b5b84196660b4bfcba57e3266d81bc667
Candace <candacelazarou@gmail.com>
2015-02-05 19:33:14 -0800

    validating email field

--- validation.js
+++ validation.js
@@ -1,16 +1,26 @@
 
 
-function isEmpty() {
+function isEmpty(inputName) {
     "use strict";
-	var x = document.forms["contact_form"]["user_name"].value;
+	var x = document.forms["contact_form"][inputName].value;
 	if ((x === null) || (x === "")) {
 		alert("empty field");
 		return false;
 	}
 }
 
-
-
-function isWorking(){
-    alert ("this works");
+function isEmail(inputName) {
+    "use strict";
+    
+    var x = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/;
+    var email = document.form["contact_form"][inputName].value;
+    
+    if (email.test(x)) {
+        alert("is a valid email");
+        return true;
+    } 
+    else {   
+        alert("is not valid email");
+        return false;
+    }
 }
